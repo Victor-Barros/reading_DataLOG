026ca28e384306e1c379e1a21599cbbaa80efdbd
Victor-Barros <victorbarroscoch@gmail.com>
2020-11-05 11:46:32 -0300

    added time and angle conversion

--- reading.py
+++ reading.py
@@ -24,7 +24,7 @@ print("Reading Sector 2 Data...")
 
 values=[[0],[0],[0],[0],[0],[0],[0],[0]] #to store analog data
 d_values=[0b00000000000000000] #to store digital data
-
+time_values=[-20] #timestamp
 
 offset=512 #offset for sector 2 (512)
 nread=1<<20 #limit of iterations 2^20
@@ -47,8 +47,10 @@ for i in range(0,2*9*nread,2): #for each 9 lines of  16 bits
 		#read analog channel and data
 		channel = word>>13
 		data = word&0x1FFF
+		#convert to degrees
+		store_angle = (data-4096)*180/4096
 		#store readings
-		values[channel].append(data)
+		values[channel].append(store_angle)
 		#print the high 3 (channel) and low 13 (data) values
 		#print("Channel: "+str(channel)+", Value: "+str(data))
 	else:
@@ -57,6 +59,9 @@ for i in range(0,2*9*nread,2): #for each 9 lines of  16 bits
 		d_values.append(word)
 		#print the digital readings
 		#print("Digital: "+str(format(word,"016b")))
+
+		#store time vector in ms
+		time_values.append(time_values[-1]+20)
 		#print("-------------------------")
 
 if eof_flag == 0:
@@ -70,11 +75,11 @@ with open(sys.argv[1]+".csv", "w") as out_file:
 	data_writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	
 	#header row
-	data_writer.writerow(["ch0","ch1","ch2","ch3","ch4","ch5","ch6","ch7","d"])
+	data_writer.writerow(["ch0","ch1","ch2","ch3","ch4","ch5","ch6","ch7","d","time(ms)"])
 	
 	#add an entry for each channel, ignoring the first (zeros) and last (usually incomplete) reading
 	for i in range(1,len(values[0])-2):
-		data_writer.writerow([values[0][i],values[1][i],values[2][i],values[3][i],values[4][i],values[5][i],values[6][i],values[7][i],format(d_values[i],"016b")])
+		data_writer.writerow([values[0][i],values[1][i],values[2][i],values[3][i],values[4][i],values[5][i],values[6][i],values[7][i],format(d_values[i],"016b"),time_values[i]])
 
 print("Saved to: "+sys.argv[1]+".csv")
 
